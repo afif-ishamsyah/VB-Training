b79e91fa7430bbf2895b8dd5d0845b3e7f6729a5
minymanz <afifhan95@gmail.com>
2018-06-07 11:39:31 +0700

    Customer Import Done Temporary

---
+++ Customer Import/Customer Import/Customer Import/Form1.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FileLabel
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.SearchButton = New System.Windows.Forms.Button()
+        Me.SearchDialog = New System.Windows.Forms.OpenFileDialog()
+        Me.CancelButton = New System.Windows.Forms.Button()
+        Me.DatabaseBox = New System.Windows.Forms.ComboBox()
+        Me.UploadButton = New System.Windows.Forms.Button()
+        Me.FileNameTextbox = New System.Windows.Forms.TextBox()
+        Me.LoadingLabel = New System.Windows.Forms.Label()
+        Me.DatabaseLabel = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'SearchButton
+        '
+        Me.SearchButton.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.SearchButton.Location = New System.Drawing.Point(12, 77)
+        Me.SearchButton.Margin = New System.Windows.Forms.Padding(2)
+        Me.SearchButton.Name = "SearchButton"
+        Me.SearchButton.Size = New System.Drawing.Size(112, 41)
+        Me.SearchButton.TabIndex = 0
+        Me.SearchButton.Text = "Search File"
+        Me.SearchButton.UseVisualStyleBackColor = True
+        '
+        'CancelButton
+        '
+        Me.CancelButton.Location = New System.Drawing.Point(264, 92)
+        Me.CancelButton.Margin = New System.Windows.Forms.Padding(2)
+        Me.CancelButton.Name = "CancelButton"
+        Me.CancelButton.Size = New System.Drawing.Size(112, 26)
+        Me.CancelButton.TabIndex = 1
+        Me.CancelButton.Text = "Cancel"
+        Me.CancelButton.UseVisualStyleBackColor = True
+        '
+        'DatabaseBox
+        '
+        Me.DatabaseBox.FormattingEnabled = True
+        Me.DatabaseBox.Items.AddRange(New Object() {"CMWTRN", "LIPDAT", "KLYBVI", "KLYKKI", "KLYKPL"})
+        Me.DatabaseBox.Location = New System.Drawing.Point(66, 12)
+        Me.DatabaseBox.Name = "DatabaseBox"
+        Me.DatabaseBox.Size = New System.Drawing.Size(185, 21)
+        Me.DatabaseBox.TabIndex = 2
+        Me.DatabaseBox.Text = "Select Database"
+        '
+        'UploadButton
+        '
+        Me.UploadButton.Location = New System.Drawing.Point(139, 77)
+        Me.UploadButton.Margin = New System.Windows.Forms.Padding(2)
+        Me.UploadButton.Name = "UploadButton"
+        Me.UploadButton.Size = New System.Drawing.Size(112, 41)
+        Me.UploadButton.TabIndex = 3
+        Me.UploadButton.Text = "Upload File"
+        Me.UploadButton.UseVisualStyleBackColor = True
+        '
+        'FileNameTextbox
+        '
+        Me.FileNameTextbox.Location = New System.Drawing.Point(41, 44)
+        Me.FileNameTextbox.Name = "FileNameTextbox"
+        Me.FileNameTextbox.Size = New System.Drawing.Size(210, 20)
+        Me.FileNameTextbox.TabIndex = 4
+        '
+        'LoadingLabel
+        '
+        Me.LoadingLabel.AutoSize = True
+        Me.LoadingLabel.BackColor = System.Drawing.SystemColors.Control
+        Me.LoadingLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LoadingLabel.Location = New System.Drawing.Point(274, 38)
+        Me.LoadingLabel.Name = "LoadingLabel"
+        Me.LoadingLabel.Size = New System.Drawing.Size(102, 13)
+        Me.LoadingLabel.TabIndex = 5
+        Me.LoadingLabel.Text = "PLEASE WAIT..."
+        '
+        'DatabaseLabel
+        '
+        Me.DatabaseLabel.AutoSize = True
+        Me.DatabaseLabel.Location = New System.Drawing.Point(9, 15)
+        Me.DatabaseLabel.Name = "DatabaseLabel"
+        Me.DatabaseLabel.Size = New System.Drawing.Size(53, 13)
+        Me.DatabaseLabel.TabIndex = 6
+        Me.DatabaseLabel.Text = "Database"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 47)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(23, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "File"
+        '
+        'FileLabel
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(389, 129)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.DatabaseLabel)
+        Me.Controls.Add(Me.LoadingLabel)
+        Me.Controls.Add(Me.FileNameTextbox)
+        Me.Controls.Add(Me.UploadButton)
+        Me.Controls.Add(Me.DatabaseBox)
+        Me.Controls.Add(Me.CancelButton)
+        Me.Controls.Add(Me.SearchButton)
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "FileLabel"
+        Me.Text = "Customer Import"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SearchButton As Button
+    Friend WithEvents SearchDialog As OpenFileDialog
+    Friend WithEvents CancelButton As Button
+    Friend WithEvents DatabaseBox As ComboBox
+    Friend WithEvents UploadButton As Button
+    Friend WithEvents FileNameTextbox As TextBox
+    Friend WithEvents LoadingLabel As Label
+    Friend WithEvents DatabaseLabel As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Customer Import/Customer Import/Customer Import/Form1.vb
@@ -0,0 +1,129 @@
+﻿Imports System.IO
+Imports ACCPAC.Advantage
+Imports Microsoft.Office.Interop
+
+Public Class FileLabel
+
+    Dim FileExcel As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        FileNameTextbox.ReadOnly = True
+        UploadButton.Enabled = False
+        LoadingLabel.Visible = False
+    End Sub
+
+    Private Sub UploadButton_Click(sender As Object, e As EventArgs) Handles UploadButton.Click
+        LoadingLabel.Visible = True
+        SenDatoSage(FileExcel)
+    End Sub
+
+    Private Sub SearchButton_Click(sender As Object, e As EventArgs) Handles SearchButton.Click
+        If SearchDialog.ShowDialog() = DialogResult.OK Then
+            FileExcel = SearchDialog.FileName
+            FileNameTextbox.Text = Path.GetFileName(FileExcel)
+            If FileNameTextbox.Text <> "" Then
+                UploadButton.Enabled = True
+            End If
+        End If
+
+    End Sub
+
+    Private Sub CancelButton_Click(sender As Object, e As EventArgs) Handles CancelButton.Click
+        End
+    End Sub
+
+
+
+    Private Sub SenDatoSage(Path As String)
+        SearchButton.Enabled = False
+        CancelButton.Enabled = False
+        UploadButton.Enabled = False
+        DatabaseBox.Enabled = False
+        Dim xlApp As Excel.Application = New Excel.Application
+        Dim xlWorkBook As Excel.Workbook
+        Dim xlWorkSheet As Excel.Worksheet
+
+
+        Dim session As Session
+        Dim mDBLinkCmpRW As DBLink
+
+        xlWorkBook = xlApp.Workbooks.Open(Path)
+        xlWorkSheet = xlWorkBook.Worksheets("Customer Creation Form")
+
+        Try
+            session = New Session()
+            session.Init("", "XX", "XX1000", "63A") 'first 3 parameter is always like that i dont know why, 4th parameter is Sage Version
+            session.Open("ADMIN", "ADMS4G3COM1", DatabaseBox.SelectedItem.ToString(), DateTime.Today, 0) 'Password and Username must be in UPPERCASE
+            mDBLinkCmpRW = session.OpenDBLink(DBLinkType.Company, DBLinkFlags.ReadWrite)
+
+            Dim ARCUSTOMER1header As View
+            Dim ARCUSTOMER1detail As View
+            Dim ARCUSTSTAT2 As View
+            Dim ARCUSTCMT3 As View
+
+            ARCUSTOMER1header = mDBLinkCmpRW.OpenView("AR0024")
+            ARCUSTOMER1detail = mDBLinkCmpRW.OpenView("AR0400")
+            ARCUSTSTAT2 = mDBLinkCmpRW.OpenView("AR0022")
+            ARCUSTCMT3 = mDBLinkCmpRW.OpenView("AR0021")
+
+            ARCUSTOMER1header.Compose({ARCUSTOMER1detail})
+            ARCUSTOMER1detail.Compose({ARCUSTOMER1header})
+
+            'INSERT DATA TO ACCPAC
+
+            ARCUSTOMER1header.Init()
+            ARCUSTOMER1header.Fields.FieldByName("IDGRP").SetValue("DP", False)
+            ARCUSTOMER1header.Fields.FieldByName("IDCUST").SetValue("CUST0001", False)
+            ARCUSTOMER1header.Fields.FieldByName("NAMECUST").SetValue(xlWorkSheet.Cells(17, 2).value, False)
+            ARCUSTOMER1header.Fields.FieldByName("TEXTSTRE1").SetValue(xlWorkSheet.Cells(25, 2).value, False)
+            ARCUSTOMER1header.Fields.FieldByName("CODEPSTL").SetValue(xlWorkSheet.Cells(31, 2).value, False)
+            ARCUSTOMER1header.Fields.FieldByName("CODECTRY").SetValue(xlWorkSheet.Cells(31, 5).value, False)
+            ARCUSTOMER1header.Fields.FieldByName("TEXTPHON1").SetValue(xlWorkSheet.Cells(33, 2).value, False)
+            ARCUSTOMER1header.Fields.FieldByName("TEXTPHON2").SetValue(xlWorkSheet.Cells(33, 5).value, False)
+            ARCUSTOMER1header.Fields.FieldByName("EMAIL2").SetValue(xlWorkSheet.Cells(29, 2).value, False)
+            ARCUSTOMER1header.Fields.FieldByName("CTACPHONE").SetValue(xlWorkSheet.Cells(47, 2).value, False)
+            ARCUSTOMER1header.Fields.FieldByName("EMAIL1").SetValue(xlWorkSheet.Cells(45, 2).value, False)
+            ARCUSTOMER1header.Fields.FieldByName("NAMECTAC").SetValue(xlWorkSheet.Cells(41, 2).value, False)
+            ARCUSTOMER1header.Insert()
+
+            MsgBox("Customer berhasil ditambah", 0, "Completed")
+
+            SearchButton.Enabled = True
+            CancelButton.Enabled = True
+            UploadButton.Enabled = True
+            DatabaseBox.Enabled = True
+            LoadingLabel.Visible = False
+
+            xlWorkBook.Close()
+            xlApp.Quit()
+
+        Catch e As NullReferenceException
+            MsgBox("Mohon pilih Database", 0, "Error")
+
+            SearchButton.Enabled = True
+            CancelButton.Enabled = True
+            UploadButton.Enabled = True
+            DatabaseBox.Enabled = True
+            LoadingLabel.Visible = False
+
+        Catch e As Runtime.InteropServices.COMException
+            Dim errors As String = ""
+
+            For k As Integer = 0 To session.Errors.Count() - 1
+                errors = errors + session.Errors(k).Message
+            Next
+
+            MessageBox.Show(errors)
+
+            End
+
+        Catch e As Exception
+            MsgBox("Error" + e.ToString(), 0, "Error")
+
+            End
+
+        End Try
+    End Sub
+
+
+End Class
